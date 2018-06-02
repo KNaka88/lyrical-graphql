b2be07c8eab5c79975786dac8c5cd6230d4bf019
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-06-02 12:02:23 -0700

    add delete song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import query from '../queries/fetchSongs';
+import fetchSongs from '../queries/fetchSongs';
+import deleteSong from '../queries/deleteSong';
 
 
 class SongList extends Component {
 
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderSongs() {
 
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({id, title}) => {
       return (
-        <li key={song.id} className="collection-item" >
-          {song.title}
+        <li key={id} className="collection-item" >
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>delete</i>
         </li>
       );
     });
@@ -39,4 +45,6 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+export default graphql(deleteSong)(
+  graphql(fetchSongs)(SongList)
+);
